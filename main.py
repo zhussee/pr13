4866da29eaa80057ba550d91a0692f64da22ced3
Bekzat Yussubayev <beka.akzhulov@gmail.com>
2026-04-22 20:20:50 +0500

    Feature implementation

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def print_hi(name):
 
     print(f'Hi, {name}')
 
+def print_hi_name(name):
+    print(f'Hi, {name}')
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_hi(input('Enter name:'))
